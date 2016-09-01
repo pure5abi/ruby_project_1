c4aac163ab7549f1272af493296c03d1882034f8
pure5abi <harsimran.mann.it@gmail.com>
2016-09-01 00:29:54 -0400

    update README

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string
+  	
+  		minutes = @seconds/60
+  		hours =  minutes/60
+  		minutes = minutes%60
+  		seconds = @seconds%60
+
+  		hours_to_s = "%02d" %hours
+  		minutes_to_s = "%02d" %minutes
+  		seconds_to_s = "%02d" %seconds
+  		
+  		result = "#{hours_to_s}:#{minutes_to_s}:#{seconds_to_s}"  	
+  end
+
 end
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # ruby_project_1
+
 Project Title: Ruby
 
 Project Link: http://www.theodinproject.com/web-development-101/ruby?ref=lnav
+
+Added Solution to:
+
+00_hello	  
+01_temperature		
+02_calculator		
+03_simon_says	
+04_pig_latin
+05_book_titles
+06_timer
+
+Project completed: September 1, 2016
